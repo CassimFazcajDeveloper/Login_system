aae35b61141ccd8bfddba3344ddc27f3c2d518fd
CassimFazcajDeveloper <cassimmikochi@gmail.com>
2026-07-30 09:40:01 +0200

    Add users and check errors implementation

--- src/index.js
+++ src/index.js
@@ -1,18 +1,18 @@
 const express = require("express");
 const path = require("path");
 const bcrypt = require("bcrypt");
-const collection = require("./config")
+const collection = require("./config");
 
 const app = express();
 
 // Set EJS
 app.set("view engine", "ejs");
-app.use(express.static("public"))
-app.use(express.json())
-app.use((express.urlencoded({extended: false})))
+app.use(express.static("public"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 // Routes
-app.get("/", (req, res) => {
+app.get("/login", (req, res) => {
   res.render("login");
 });
 
@@ -20,7 +20,7 @@ app.get("/signup", (req, res) => {
   res.render("signup");
 });
 
-
+// methods for posting data into database
 app.post("/signup", async (req, res) => {
   try {
     const hashedPassword = await bcrypt.hash(req.body.password, 10);
@@ -30,20 +30,43 @@ app.post("/signup", async (req, res) => {
       password: hashedPassword,
     };
 
-    const userData = await collection.insertMany(data);
-
-    console.log(userData);
-
-    res.send("User registered successfully");
+    const existinguser = await collection.findOne({ name: data.name });
+    if (existinguser) {
+      res.send("user alredy exits");
+    } else {
+      // send data to databse collection
+      const userData = await collection.insertMany(data);
+      res.send("User registered successfully");
+    }
   } catch (err) {
     console.error(err);
     res.status(500).send("Something went wrong");
   }
 });
 
-// Start server
-const port = 5000;
+app.post("/login", async (req, res) => {
+  try {
+    const check = await collection.findOne({ name: req.body.username });
+    if (!check) {
+      res.send("user cannot be found");
+    }
+    // compare the pasword from that of databse encrypted
+    const ispasswordMatch = await bcrypt.compare(
+      req.body.password,
+      check.password,
+    );
+    if (ispasswordMatch) {
+      res.render("home");
+    } else {
+      res.send("wrong password");
+    }
+  } catch {
+    res.send("wrongndetails");
+  }
+});
 
+// define port and Start server
+const port = 5000;
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
